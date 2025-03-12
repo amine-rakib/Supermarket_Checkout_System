c95f73eadf4f5204dafa8ccb3d9bb3ef1f03c1da
amine <amine.rakib@student-cs.fr>
2025-03-12 15:37:39 +0100

    Corrections des signatures de fcts

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -8,10 +8,9 @@ public class Customer {
     private String plan; // normal, prime, platinium
     private double x_axis;
     private double y_axis;
-    private String payment_method;
-    private String pinCode;
+    private BankCard BankCard;
 
-    public Customer(String firstName, String lastName, String plan, double x_axis, double y_axis, String payment_method, String pinCode) {
+    public Customer(String firstName, String lastName, String plan, double x_axis, double y_axis, BankCard BankCard) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.id = generateId();
@@ -21,8 +20,7 @@ public class Customer {
         this.x_axis = x_axis;
         this.y_axis = y_axis;
 
-        this.payment_method = payment_method;
-        this.pinCode = pinCode;
+        this.BankCard = BankCard;
     }
 
     private UUID generateId() {
@@ -41,9 +39,7 @@ public class Customer {
     public double getX_axis() { return x_axis; }
     public double getY_axis() { return y_axis; }
 
-    public String getPaymentMethod() { return payment_method; }
-
-    public String getPinCode() { return pinCode; }
+    public BankCard getBankCard() { return BankCard; }
 
 }
 
